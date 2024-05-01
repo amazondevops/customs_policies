fac354c19af9ebb6aa85d3ccf4fd2458aacc414a
SreekanthReddyy <sreekanth.reddyy@jungleegames.com>
2024-05-01 11:39:12 +0530

    initia commit

--- s3/S3PCIPrivateACL.py
+++ s3/S3PCIPrivateACL.py
@@ -9,9 +9,9 @@ from checkov.common.models.enums import CheckResult, CheckCategories
 class S3PCIPrivateACL(BaseResourceCheck):
     def __init__(self) -> None:
         # Initialize with a generic name
-        self.name = "Ensure Bucket must contain a tag called product_v2 and terraform_managed"
-        id = "JG_TF_01"
-        supported_resources = ("aws_s3_bucket",)
+        self.name = "Ensure Resource must contain a tag called product_v2 and terraform_managed"
+        id = "JG_AWS_TGA_01"
+        supported_resources = ("aws_s3_bucket","aws_instance","aws_vpc","aws_subnet","aws_network_interface","aws_security_group","s3_bucket","security_group",)
         categories = (CheckCategories.BACKUP_AND_RECOVERY,)
         guideline = "Follow the link to get more info https://docs.prismacloud.io/en/enterprise-edition/policy-reference"
         super().__init__(name=self.name, id=id, categories=categories, supported_resources=supported_resources, guideline=guideline)
@@ -24,8 +24,9 @@ class S3PCIPrivateACL(BaseResourceCheck):
         :return: <CheckResult>
         """
         tags = conf.get("tags")
+        #print(tags)
         if tags is None or (isinstance(tags, list) and len(tags) == 0) or (isinstance(tags, list) and isinstance(tags[0], dict) and len(tags[0]) == 0):
-            self.name = "No tags found for the S3 bucket"
+            #self.name = "No tags found for the S3 bucket"
             return CheckResult.FAILED
 
         # Assuming you only have one dictionary in the list
@@ -34,9 +35,11 @@ class S3PCIPrivateACL(BaseResourceCheck):
         required_tags = ["product_v2", "terraform_managed"]
         for req_tag in required_tags:
             if req_tag not in tag_dict:
-                self.name = f"Tag '{req_tag}' not found for the S3 bucket"
+                #self.name = f"Tag '{req_tag}' not found for the S3 bucket"
                 return CheckResult.FAILED
 
         return CheckResult.PASSED
 
 check = S3PCIPrivateACL()
+
+
---
+++ sg/SecurityGroupUnrestrictedIngress80.py
@@ -0,0 +1,37 @@
+from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
+from checkov.common.models.enums import CheckResult, CheckCategories
+
+class NonPublicPort80Check(BaseResourceCheck):
+    def __init__(self) -> None:
+        name = "Ensure AWS security groups do not have port 80 open to everywhere"
+        id = "JG_AWS_SG_O1"
+        supported_resources = ("aws_security_group","security_group","module.security_group","aws_security_group_rule",)
+        categories = (CheckCategories.NETWORKING,)
+        guideline = "Port 80 should not be open to everywhere in security group rules."
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources, guideline=guideline)
+
+    def scan_resource_conf(self, conf) -> CheckResult:
+        print(conf)
+        ingress_rules = conf.get("ingress")
+        #print("***",ingress_rules)
+        if ingress_rules:
+            for rule in ingress_rules:
+                from_ports = rule.get("from_port")
+                to_ports = rule.get("to_port")
+                # Ensure from_ports and to_ports are lists
+                if isinstance(from_ports, list) and isinstance(to_ports, list):
+                    for from_port, to_port in zip(from_ports, to_ports):
+                        # Check if port 80 is open to 0.0.0.0/0
+                        if from_port == 80 and to_port == 80:
+                            cidr_blocks = rule.get("cidr_blocks")
+                            if cidr_blocks and any("0.0.0.0/0" in block for block in cidr_blocks):
+                                return CheckResult.FAILED
+                                break  # Exit the inner loop
+                    else:
+                        continue  # Continue to the next rule if port 80 check failed
+                    break  # Exit the outer loop if port 80 check passed
+            else:
+                return CheckResult.PASSED
+
+non_public_port_80_check = NonPublicPort80Check()
+
